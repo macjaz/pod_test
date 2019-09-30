4e39d30925ee9805cc3b34723dabe201647fb5b8
macjaz <macjaz@163.com>
2019-10-01 05:16:42 +0800

    第一次提交

---
+++ pod_test/Classes/NSString+Test.h
@@ -0,0 +1,21 @@
+//
+//  NSString+Test.h
+//  MyTestSDK
+//
+//  Created by wcj on 2019/10/1.
+//  Copyright © 2019 wcj. All rights reserved.
+//
+
+
+
+
+@interface NSString (Test)
+
+
+- (void)test;
+
+
+
+
+@end
+
---
+++ pod_test/Classes/NSString+Test.m
@@ -0,0 +1,17 @@
+//
+//  NSString+Test.m
+//  MyTestSDK
+//
+//  Created by wcj on 2019/10/1.
+//  Copyright © 2019 wcj. All rights reserved.
+//
+
+#import "NSString+Test.h"
+
+@implementation NSString (Test)
+
+- (void)test{
+    NSLog(@"这是一个测试");
+}
+
+@end
